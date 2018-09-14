9be3be5dc057278807a899445e78afd6b0f5b1eb
Suwen Ge <gesuwen@live.cn>
2018-09-13 21:03:47 -0400

    2018.9.13

---
+++ 118 Pascal's Triangle.py
@@ -0,0 +1,34 @@
+# Array
+
+# Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+#
+#
+# In Pascal's triangle, each number is the sum of the two numbers directly above it.
+#
+# Example:
+#
+# Input: 5
+# Output:
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+from operator import add
+
+class Solution:
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0:
+            return []
+        currentRow = [1]
+        output = [currentRow]
+        for i in range(1, numRows):
+            currentRow = list( map(add, [0]+currentRow, currentRow+[0]))
+            output = output + [currentRow]
+        return output

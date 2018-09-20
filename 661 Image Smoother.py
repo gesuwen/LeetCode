2fd5dd81db3219474a1bb7fb62d86b0e967a36fe
Suwen Ge <gesuwen@live.cn>
2018-09-19 22:09:28 -0400

    2018.9.19

---
+++ 661 Image Smoother.py
@@ -0,0 +1,40 @@
+# Array
+
+# Given a 2D integer matrix M representing the gray scale of an image, you need to design a smoother to make the gray scale of each cell becomes the average gray scale (rounding down) of all the 8 surrounding cells and itself. If a cell has less than 8 surrounding cells, then use as many as you can.
+#
+# Example 1:
+# Input:
+# [[1,1,1],
+#  [1,0,1],
+#  [1,1,1]]
+# Output:
+# [[0, 0, 0],
+#  [0, 0, 0],
+#  [0, 0, 0]]
+# Explanation:
+# For the point (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+# For the point (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+# For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+# Note:
+# The value in the given matrix is in the range of [0, 255].
+# The length and width of the given matrix are in the range of [1, 150].
+class Solution:
+    def imageSmoother(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        rowLen = len(M)
+        colLen = len(M[0])
+        output = [[0 for i in range(colLen)] for j in range(rowLen)]
+        for i in range(rowLen):
+            for j in range(colLen):
+                count = 0
+                for r in (i-1, i, i+1):
+                    for c in (j-1, j, j+1):
+                        if 0 <= r < rowLen and 0 <= c < colLen:
+                            output[i][j] += M[r][c]
+                            count += 1
+                output[i][j] /= count
+                output[i][j] = int(output[i][j])
+        return output

f28968c6862cc8e39d835ba60f6b9735da668020
Suwen Ge <gesuwen@live.cn>
2018-10-15 20:48:13 -0400

    2018.10.15

---
+++ 073 Set Matrix Zeroes.py
@@ -0,0 +1,61 @@
+# Array
+
+# Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
+#
+# Example 1:
+#
+# Input:
+# [
+#   [1,1,1],
+#   [1,0,1],
+#   [1,1,1]
+# ]
+# Output:
+# [
+#   [1,0,1],
+#   [0,0,0],
+#   [1,0,1]
+# ]
+# Example 2:
+#
+# Input:
+# [
+#   [0,1,2,0],
+#   [3,4,5,2],
+#   [1,3,1,5]
+# ]
+# Output:
+# [
+#   [0,0,0,0],
+#   [0,4,5,0],
+#   [0,3,1,0]
+# ]
+# Follow up:
+#
+# A straight forward solution using O(mn) space is probably a bad idea.
+# A simple improvement uses O(m + n) space, but still not the best solution.
+# Could you devise a constant space solution?
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        is_col = False
+        matWid, matLen = len(matrix), len(matrix[0])
+        for i in range(matWid):
+            if matrix[i][0] == 0:
+                is_col = True
+            for j in range(1, matLen):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+        for i in range(1, matWid):
+            for j in range(1, matLen):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if matrix[0][0] == 0:
+            for j in range(matLen):
+                matrix[0][j] = 0
+        if is_col:
+            for i in range(matWid):
+                matrix[i][0] = 0

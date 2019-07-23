b02035c67ab37d403ccced3bb1b9b6bdbf8f30a8
gesuwen <gesuwen@hotmail.com>
2019-07-22 22:27:30 -0400

    Revert "2019.7.22"
    This reverts commit 8f5f19108743c1119efd2a3023d8276d3e6d6a1b.
    
    2019.7.22

---
+++ LeetCode/099 Recover Binary Search Tree.java
@@ -0,0 +1,99 @@
+// Tree; Depth First Search
+
+// Two elements of a binary search tree (BST) are swapped by mistake.
+
+// Recover the tree without changing its structure.
+
+// Example 1:
+
+// Input: [1,3,null,null,2]
+
+//    1
+//   /
+//  3
+//   \
+//    2
+
+// Output: [3,1,null,null,2]
+
+//    3
+//   /
+//  1
+//   \
+//    2
+// Example 2:
+
+// Input: [3,1,4,null,null,2]
+
+//   3
+//  / \
+// 1   4
+//    /
+//   2
+
+// Output: [2,1,4,null,null,3]
+
+//   2
+//  / \
+// 1   4
+//    /
+//   3
+// Follow up:
+
+// A solution using O(n) space is pretty straight forward.
+// Could you devise a constant space solution?
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void recoverTree(TreeNode root) {
+        TreeNode pre = null;
+        TreeNode first = null, second = null;
+        // Morris Traversal
+        TreeNode temp = null;
+		while(root!=null){
+			if(root.left!=null){
+				// connect threading for root
+				temp = root.left;
+				while(temp.right!=null && temp.right != root)
+					temp = temp.right;
+				// the threading already exists
+				if(temp.right!=null){
+				    if(pre!=null && pre.val > root.val){
+				        if(first==null){first = pre;second = root;}
+				        else{second = root;}
+				    }
+				    pre = root;
+				    
+					temp.right = null;
+					root = root.right;
+				}else{
+					// construct the threading
+					temp.right = root;
+					root = root.left;
+				}
+			}else{
+				if(pre!=null && pre.val > root.val){
+				    if(first==null){first = pre;second = root;}
+				    else{second = root;}
+				}
+				pre = root;
+				root = root.right;
+			}
+		}
+		// swap two node values;
+		if(first!= null && second != null){
+		    int t = first.val;
+		    first.val = second.val;
+		    second.val = t;
+		}
+    }
+
+}

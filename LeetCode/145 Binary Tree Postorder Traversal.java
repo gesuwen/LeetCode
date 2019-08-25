2b5346ce142790547ec408f0a5cd0e57bffe7cc0
Suwen Ge <gesuwen@hotmail.com>
2019-08-25 08:33:25 -0400

    2019.8.24

---
+++ LeetCode/145 Binary Tree Postorder Traversal.java
@@ -0,0 +1,44 @@
+// Stack; Tree 
+
+// Given a binary tree, return the postorder traversal of its nodes' values.
+
+// Example:
+
+// Input: [1,null,2,3]
+//    1
+//     \
+//      2
+//     /
+//    3
+
+// Output: [3,2,1]
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        LinkedList<Integer> result = new LinkedList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        TreeNode p = root;
+        while (!stack.isEmpty() || p != null) {
+            if (p != null) {
+                stack.push(p);
+                result.addFirst(p.val);
+                p = p.right;
+            }
+            else {
+                TreeNode node = stack.pop();
+                p = node.left;
+            }
+        }
+        return result;
+    }
+}

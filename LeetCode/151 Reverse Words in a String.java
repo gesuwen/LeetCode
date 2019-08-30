e10d8eac1da84a57d280446d12541df08e213307
Suwen Ge <gesuwen@hotmail.com>
2019-08-29 23:46:36 -0400

    2019.8.30

---
+++ LeetCode/151 Reverse Words in a String.java
@@ -0,0 +1,71 @@
+// String
+
+// Given an input string, reverse the string word by word.
+
+ 
+
+// Example 1:
+
+// Input: "the sky is blue"
+// Output: "blue is sky the"
+// Example 2:
+
+// Input: "  hello world!  "
+// Output: "world! hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+// Example 3:
+
+// Input: "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+// Note:
+
+// A word is defined as a sequence of non-space characters.
+// Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+// You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+// Follow up:
+
+// For C programmers, try to solve it in-place in O(1) extra space.
+
+class Solution {
+    public String reverseWords(String s) {
+        if (s == null) return null;
+        char[] a = s.toCharArray();
+        int n = a.length;
+        reverse(a, 0, n-1);
+        reverseWords(a, n);
+        return cleanSpaces(a, n);
+    }
+    
+    void reverseWords(char[] a, int n) {
+        int i = 0, j = 0;
+        while (i < n) {
+            while (i < j || i < n && a[i] == ' ') i++;
+            while (j < i || j < n && a[j] != ' ') j++;
+            reverse(a, i, j - 1);
+        }
+    }
+    
+    String cleanSpaces(char[] a, int n) {
+        int i = 0, j = 0;
+        while (j < n) {
+            while (j < n && a[j] == ' ') j++;
+            while (j < n && a[j] != ' ') a[i++] = a[j++];
+            while (j < n && a[j] == ' ') j++;
+            if (j < n) a[i++] = ' ';
+        }
+        return new String(a).substring(0, i);
+    }
+    
+    private void reverse(char[] a, int i, int j) {
+        while (i < j) {
+            char t = a[i];
+            a[i++] = a[j];
+            a[j--] = t;
+        }
+    }
+}
---
+++ LeetCode/155 Min Stack.java
@@ -0,0 +1,70 @@
+// Stack; Design
+
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// push(x) -- Push element x onto stack.
+// pop() -- Removes the element on top of the stack.
+// top() -- Get the top element.
+// getMin() -- Retrieve the minimum element in the stack.
+ 
+
+// Example:
+
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin();   --> Returns -3.
+// minStack.pop();
+// minStack.top();      --> Returns 0.
+// minStack.getMin();   --> Returns -2.
+
+class MinStack {
+    /** initialize your data structure here. */
+    long min;
+    Stack<Long> stack;
+    
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (stack.isEmpty()) {
+            stack.push(0L);
+            min = x;
+        }
+        else {
+            stack.push(x - min);
+            if (x < min) min = x;
+        }
+    }
+    
+    public void pop() {
+        if (stack.isEmpty()) return;
+        long pop = stack.pop();
+        if (pop < 0) min = min - pop;
+    }
+    
+    public int top() {
+        long top = stack.peek();
+        if (top > 0) {
+            return (int) (top + min);
+        }
+        else {
+            return (int) min;
+        }
+    }
+    
+    public int getMin() {
+        return (int) min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
